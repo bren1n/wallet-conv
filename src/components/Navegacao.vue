cadf2a41baee6546240625e3d666ad8cadac70a0
Pedro Vicente <pedrov.nana@gmail.com>
2021-08-27 21:33:25 -0300

    Add the navigation, pages and chart extension

---
+++ src/components/Navegacao.vue
@@ -0,0 +1,46 @@
+<template>
+      <v-card>
+        <v-navigation-drawer
+          permanent
+          expand-on-hover
+          app
+          floating
+          color="primary"
+        >
+          <v-list-item class="pa-2">
+            <v-img src="../assets/logo_fundo.png"></v-img>
+          </v-list-item>
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item link to="/">
+                <v-list-item-icon>
+                  <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item link to="/gastos">
+                <v-list-item-icon>
+                  <v-icon>mdi-shopping</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Gastos</v-list-item-title>
+            </v-list-item>
+            <v-list-item link to="/receitas">
+                <v-list-item-icon>
+                  <v-icon>mdi-trending-up</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Receitas</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Navegacao',
+
+    data: () => ({}),
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <apexchart type="donut" width="380" :options="chartOptions" :series="series"></apexchart>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import VueApexCharts from 'vue-apexcharts'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    apexchart: VueApexCharts
+  },
+  data: function() {
+      return {
+        series: [44, 55, 13, 33],
+        chartOptions: {
+          chart: {
+            width: 380,
+            type: 'donut',
+          },
+          dataLabels: {
+            enabled: false
+          },
+          responsive: [{
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200
+              },
+              legend: {
+                show: false
+              }
+            }
+          }],
+          legend: {
+            position: 'right',
+            offsetY: 0,
+            height: 230,
+          }
+        },
+      }
+    },
 }
 </script>
